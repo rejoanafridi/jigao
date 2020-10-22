ee7b344201bded63a243ce58340ef4a5dcde0d7f
Sakib Ahmad Siddiquee <sakib15-10776@diu.edu.bd>
2020-10-23 03:03:48 +0600

    superuser and login created
    superuser crated
    
    username: admin
    password: 1234
    
    Only login created. Registration is pending

--- account/views.py
+++ account/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.http import request
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.contrib.auth import forms
 
 # Create your views here.
+
+
+def index(request):
+        if request.user.is_authenticated:
+            return render(request, 'main/index.html')
+        else:
+            return render(request, 'account/login.html')
+
+def login(request):
+        if request.user.is_authenticated:
+            return render(request, 'main/index.html')
+        else:
+            if request.method == 'GET':
+                return render(request, 'account/login.html')
+
+            elif request.method == 'POST':
+                username = request.POST['username']
+                password = request.POST['password']
+
+                user = authenticate(username=username, password=password)
+                if user is not None:
+                    auth_login(request, user)
+                    return redirect('/')
+                else:
+                    return render(request, 'account/login.html', {'msg': 'Invalid username and password'})
+
+def logout(request):
+    auth_logout(request)
+    return redirect('login')
+
+def register(request):
+    return render(request,'account/register.html')
--- main/views.py
+++ main/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render, HttpResponse
+from django.http.response import HttpResponse
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'index.html')
+    if request.user.is_authenticated:
+        return render(request, "main/index.html")
+    else:
+        return redirect('account/login')
 
 
 def test(request):
     return render(request, 'base.html')
+
+
